d9511f3726f7ec375b292ef3d2f80be9e484415d
Michail Tsoma <dagobertusdagobertus@gmail.com>
2020-02-07 13:56:20 +0200

    Updates

---
+++ precache-manifest.c699f09d4003a30275d5440a45c1b854.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24c6d01be63ccc43e25331e9237760a",
+    "url": "/notepad-for-words/index.html"
+  },
+  {
+    "revision": "8357041867b89bfeeddb",
+    "url": "/notepad-for-words/static/css/main.2ac99b58.chunk.css"
+  },
+  {
+    "revision": "ca7d758adc56cef0076a",
+    "url": "/notepad-for-words/static/js/2.6f48810c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/notepad-for-words/static/js/2.6f48810c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8357041867b89bfeeddb",
+    "url": "/notepad-for-words/static/js/main.280af08d.chunk.js"
+  },
+  {
+    "revision": "897dc2ea07cf1ae231c1",
+    "url": "/notepad-for-words/static/js/runtime-main.640b44fc.js"
+  },
+  {
+    "revision": "de59a0308707d78cfe8304ad4414bfa9",
+    "url": "/notepad-for-words/static/media/Layer.de59a030.png"
+  },
+  {
+    "revision": "f856d3e21e2b8424a7f9b805f91c39bf",
+    "url": "/notepad-for-words/static/media/Linkedin.f856d3e2.png"
+  },
+  {
+    "revision": "aee4facbeae21b5826bd0040de4c3fd2",
+    "url": "/notepad-for-words/static/media/ajax-loader.aee4facb.gif"
+  },
+  {
+    "revision": "9bc9ef4d2f15253f0017056af61f1c6c",
+    "url": "/notepad-for-words/static/media/evening.9bc9ef4d.jpg"
+  },
+  {
+    "revision": "1e73d566bc48898c0f19c1f53dbbcd6a",
+    "url": "/notepad-for-words/static/media/games.1e73d566.jpg"
+  },
+  {
+    "revision": "8df07bef3c647e9907f3b6ca2680b566",
+    "url": "/notepad-for-words/static/media/git.8df07bef.png"
+  },
+  {
+    "revision": "56ea7c95fa32283336a5fafd30140d61",
+    "url": "/notepad-for-words/static/media/market.56ea7c95.jpg"
+  },
+  {
+    "revision": "93bda932d4826b3c7fd031f4955054ed",
+    "url": "/notepad-for-words/static/media/notepad.93bda932.png"
+  },
+  {
+    "revision": "37d1a7af7a43817ad8c408b1602f7f85",
+    "url": "/notepad-for-words/static/media/skype.37d1a7af.png"
+  },
+  {
+    "revision": "bcbb393bd7cae728fa4bc47a6ccdcca1",
+    "url": "/notepad-for-words/static/media/sukses.bcbb393b.jpg"
+  },
+  {
+    "revision": "d7ea36283ebae92bf1fdcf8b7469a41a",
+    "url": "/notepad-for-words/static/media/sunset.d7ea3628.jpg"
+  },
+  {
+    "revision": "f16c3c2b668e6cbea4e8d1c9ed439382",
+    "url": "/notepad-for-words/static/media/telegram.f16c3c2b.png"
+  },
+  {
+    "revision": "c312eacd9d8fb90304e5c84371b072ad",
+    "url": "/notepad-for-words/static/media/training.c312eacd.jpg"
+  },
+  {
+    "revision": "a1cfec9b9392cc4b2274a0c83c55834c",
+    "url": "/notepad-for-words/static/media/viber.a1cfec9b.png"
+  },
+  {
+    "revision": "fcfe5371db6778e5bc4e6151321ad9ea",
+    "url": "/notepad-for-words/static/media/word2.fcfe5371.jpg"
+  }
+]);
