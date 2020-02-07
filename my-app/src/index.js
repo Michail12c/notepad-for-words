513f964186206a32f5d0e650c8e5e114ddcffd2c
Michail Tsoma <dagobertusdagobertus@gmail.com>
2020-02-07 14:25:43 +0200

    'deploy'

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/redux/redux-store';
 
-ReactDOM.render(<HashRouter><Provider store = {store}><App /></Provider></HashRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter basename = {process.env.PUBLIC_URL}><Provider store = {store}><App /></Provider></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
