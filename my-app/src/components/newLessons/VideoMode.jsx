ac4c2c0bfe55d67350ab9a85e6cb1546db15464e
Michail Tsoma <dagobertusdagobertus@gmail.com>
2020-04-21 14:03:06 +0300

    added material UX develop video mode

--- my-app/src/components/newLessons/VideoMode.jsx
+++ my-app/src/components/newLessons/VideoMode.jsx
@@ -1,15 +1,88 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 import style from './NewLessons.module.css'
+import { useState } from 'react'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import PopupState, { bindTrigger, bindMenu, popupState } from 'material-ui-popup-state'
+
 
 const VideoMode = () => {
+  let masUrl = ["https://www.youtube.com/watch?v=i96VS_z8y7g", "https://www.youtube.com/watch?v=15Ucj4YFosk"]
+
+  const [activeUrl, setActiveUrl] = useState(masUrl[0])  
+  let url = activeUrl 
+
+  const choiceUrl = (callback, index) => {
+    callback()
+    setActiveUrl(masUrl[index])
+  }
+
+
+
+
   return(
-    <div>
+    <div className = {style.videoMode}>
+       <MenuVideo choiceUrl = {choiceUrl} />
+      <div>
+      <ReactPlayer  url={url}
+                    youtubeConfig={{ playerVars:{ 
+                      showinfo: 1, 
+                      controls: 2,
+                      autoplay: 1,
+                      modestbranding: 1 }}}
+                      playing />
+      </div>
        <div>
-        <b>Цей розділ знаходиться на стадії розробки </b>  
+         here will me translate
        </div>
-       <img className = {style.warning} src="https://www.nasemesto.rs/wp-content/uploads/2019/05/radovi-na-putu.jpg" alt=""/>
     </div>
   )
 }
+
+
+const MenuVideo = ({choiceUrl}) => {
+  return (
+    <div>
+         <div>
+            <PopupState variant="popover" popupId="demo-popup-menu">
+          {(popupState) => (
+          <React.Fragment>
+            <Button variant="contained" className = {style.buttonMode} color="primary" {...bindTrigger(popupState)}>
+              Легкий рівень
+            </Button>
+            <Menu {...bindMenu(popupState)}>
+          <MenuItem onClick={() => choiceUrl(popupState.close, 0)}>test</MenuItem>
+              <MenuItem onClick={() => choiceUrl(popupState.close, 1)}>Інше</MenuItem>
+          <MenuItem onClick={() => choiceUrl(popupState.close, 1)}>test2</MenuItem>
+            </Menu>
+          </React.Fragment>
+  
+        )}
+        </PopupState>
+        </div>
+
+        <div>
+            <PopupState variant="popover" popupId="demo-popup-menu">
+          {(popupState) => (
+          <React.Fragment>
+            <Button variant="contained" color="primary" {...bindTrigger(popupState)}>
+             Середній рівень
+            </Button>
+            <Menu {...bindMenu(popupState)}>
+          <MenuItem onClick={() => choiceUrl(popupState.close, 0)}>test</MenuItem>
+          <MenuItem onClick={() => choiceUrl(popupState.close, 1)}>test2</MenuItem>
+            </Menu>
+          </React.Fragment>
+  
+        )}
+        </PopupState>
+        </div>
+    </div>
+  ) 
+}
+
+
 
 export default VideoMode 
