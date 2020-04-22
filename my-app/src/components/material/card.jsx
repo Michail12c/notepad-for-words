f99a460eeaaa9fb9276443088a7ae7e47d13c960
Michail Tsoma <dagobertusdagobertus@gmail.com>
2020-04-22 14:03:28 +0300

    added tabs

---
+++ my-app/src/components/material/card.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import success from '../images/sukses.jpg'
+
+
+
+export default function MediaCard({width, height, content,  callback, cardHeight, choiceVideo}) {
+
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: width,
+      maxHeight: cardHeight,
+      marginRight: '20px',
+      marginBottom: '20px'
+    },
+    media: {
+      height: height,
+    },
+  });
+const startFunction = () => {
+  callback()
+  choiceVideo()
+}
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image= {success}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" onClick = {startFunction}>
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- my-app/src/components/newLessons/VideoMode.jsx
+++ my-app/src/components/newLessons/VideoMode.jsx
@@ -2,87 +2,142 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 import style from './NewLessons.module.css'
 import { useState } from 'react'
-import Button from '@material-ui/core/Button'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
-import PopupState, { bindTrigger, bindMenu, popupState } from 'material-ui-popup-state'
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Fade from 'react-reveal/Fade';
+import MediaCard from '../material/card';
+
 
 
 const VideoMode = () => {
-  let masUrl = ["https://www.youtube.com/watch?v=i96VS_z8y7g", "https://www.youtube.com/watch?v=15Ucj4YFosk"]
-
-  const [activeUrl, setActiveUrl] = useState(masUrl[0])  
+  let masUrl = [{url: "https://www.youtube.com/watch?v=i96VS_z8y7g", name: "Хрещенний батько"}, {url: "https://www.youtube.com/watch?v=15Ucj4YFosk", name: "Хрещенний батько 2"}]
+  const [test, setTest] = useState(false)
+  const [activeUrl, setActiveUrl] = useState(masUrl[0].url)  
   let url = activeUrl 
 
-  const choiceUrl = (callback, index) => {
-    callback()
-    setActiveUrl(masUrl[index])
-  }
-
-
-
+ 
 
   return(
     <div className = {style.videoMode}>
-       <MenuVideo choiceUrl = {choiceUrl} />
-      <div>
-      <ReactPlayer  url={url}
-                    youtubeConfig={{ playerVars:{ 
-                      showinfo: 1, 
-                      controls: 2,
-                      autoplay: 1,
-                      modestbranding: 1 }}}
-                      playing />
-      </div>
-       <div>
-         here will me translate
-       </div>
+      { !test 
+         ? <VerticalTabs setTest = {setTest} 
+                         setActiveUrl = {setActiveUrl}
+                         masUrl = {masUrl} />
+         : <Fade>
+              
+                <ReactPlayer  url={url}
+                              youtubeConfig={{ playerVars:{ 
+                                showinfo: 1, 
+                                controls: 2,
+                                autoplay: 1,
+                                modestbranding: 1 }}}
+                                playing />
+                <div>
+                  <button onClick = {() => setTest(false)}>come back</button>
+                </div>
+              </Fade>
+      }
     </div>
   )
 }
 
 
-const MenuVideo = ({choiceUrl}) => {
-  return (
-    <div>
-         <div>
-            <PopupState variant="popover" popupId="demo-popup-menu">
-          {(popupState) => (
-          <React.Fragment>
-            <Button variant="contained" className = {style.buttonMode} color="primary" {...bindTrigger(popupState)}>
-              Легкий рівень
-            </Button>
-            <Menu {...bindMenu(popupState)}>
-          <MenuItem onClick={() => choiceUrl(popupState.close, 0)}>test</MenuItem>
-              <MenuItem onClick={() => choiceUrl(popupState.close, 1)}>Інше</MenuItem>
-          <MenuItem onClick={() => choiceUrl(popupState.close, 1)}>test2</MenuItem>
-            </Menu>
-          </React.Fragment>
-  
-        )}
-        </PopupState>
-        </div>
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-        <div>
-            <PopupState variant="popover" popupId="demo-popup-menu">
-          {(popupState) => (
-          <React.Fragment>
-            <Button variant="contained" color="primary" {...bindTrigger(popupState)}>
-             Середній рівень
-            </Button>
-            <Menu {...bindMenu(popupState)}>
-          <MenuItem onClick={() => choiceUrl(popupState.close, 0)}>test</MenuItem>
-          <MenuItem onClick={() => choiceUrl(popupState.close, 1)}>test2</MenuItem>
-            </Menu>
-          </React.Fragment>
-  
-        )}
-        </PopupState>
-        </div>
-    </div>
-  ) 
+  return (
+    <Typography
+      className = {style.sectionTypography}
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
 }
 
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: "rgb(191, 216, 175)",
+    display: 'flex',
+    height: 'max-content',
+  },
+
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+ function VerticalTabs({setTest, setActiveUrl, masUrl}) {
+
+   console.log(masUrl)
+
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+ 
+  let contentMiddle = masUrl.map((el, index) =>  <MediaCard key = {index} width = {150} 
+                                          height =  {120} 
+                                          content = {el.name} 
+                                          callback = {() => setTest(true)}
+                                          cardHeight= {250}
+                                          choiceVideo = {() => setActiveUrl(el.url)} />  )
+  return (
+    <Fade>
+      <div className={classes.root}>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          className={classes.tabs}
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          <button onClick = {() => setTest(true)}>click</button>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <div className = {style.sectionRoot}>
+            {contentMiddle}
+          </div>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+      </div>
+  </Fade>
+  );
+}
 
 
 export default VideoMode 
